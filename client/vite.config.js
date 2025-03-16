160fc5c981155e220738d40edc669d76cbc4f87e
eguono imonieroh <imonieroheguono@gmail.com>
2025-03-17 00:15:39 +0100

    fixes:yup fixes in vite.config.js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,5 +8,9 @@ export default defineConfig({
   plugins: [react() ,  tailwindcss()],
   optimizeDeps: {
     include: ["yup"],
+  }, build: {
+    rollupOptions: {
+      external: ["yup"]
+    },
   },
 })
