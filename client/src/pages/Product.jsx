2c1b3cacc5a65625852fa44a4a274eb7f7bfac40
eguono imonieroh <imonieroheguono@gmail.com>
2025-04-03 15:58:59 +0100

    fixes

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -31,7 +31,7 @@ const Product = () => {
 
       const allProducts = await fetch(`https://eggys-place.onrender.com/api/product/all-products`);
       const allProductsData = await allProducts.json();
-      const filteredSimilarProducts = allProductsData.products.filter((item)=>item.category === res.product.category && item._id);
+      const filteredSimilarProducts = allProductsData.products.filter((item)=>item?.category === res.product?.category && item._id);
 
       setSimilarProducts(filteredSimilarProducts);
       
